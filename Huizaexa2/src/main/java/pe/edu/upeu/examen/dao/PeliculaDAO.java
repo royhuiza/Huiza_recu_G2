c5542bc0f6bbe1192619817a2d8177b85d9cab1d
royhuiza <webmaster123p@gmail.com>
2025-06-05 16:53:18 -0500

    Delete Huizaexa2 directory

--- Huizaexa2/src/main/java/pe/edu/upeu/examen/dao/PeliculaDAO.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upeu.examen.dao;
-
-import pe.edu.upeu.examen.entity.Pelicula;
-import pe.edu.upeu.examen.generic.GenericDAO;
-
-public interface PeliculaDAO extends GenericDAO<Pelicula>{
-
-}
